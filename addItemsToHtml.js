2c848b151624d7645c6dac6a56d0eeba0d38ce75
jfroggg <68919685+jfroggg@users.noreply.github.com>
2020-08-14 04:53:36 -0400

    Add files via upload

---
+++ addItemsToHtml.js
@@ -0,0 +1,67 @@
+import {items} from "./items.js";
+
+
+// <div class="card col-md-6">
+//                 <h5 class="card-header">Featured</h5>
+//                 <div class="card-body">
+//                   <h5 class="card-title">Special title treatment</h5>
+//                   <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+//                   <a href="#" class="btn btn-primary">Go somewhere</a>
+//                 </div>
+//               </div>
+
+
+var customRows=document.querySelector("#customRows")
+
+var customArray=[];
+var length=6;
+
+if(items.length<length){
+    length=items.length;
+}
+
+
+for(var i=0;i<length;i++){
+    findRandom(); 
+}
+
+function findRandom(){
+    var randomNumber=Math.floor(Math.random()*items.length);
+    console.log(randomNumber);
+        if(customArray.includes(items[randomNumber])){
+        findRandom();
+        }
+        else{
+        customArray.push(items[randomNumber]);
+        }
+}
+
+console.log(customArray);
+
+customArray.forEach(element => {
+   
+    var htmlString='<div col-sm-6 col-md-4><div class="card my-2 border-jason text-center shadow-lg rounded">';
+    var header="<h5 class='card-header'>"+element.header+"</h5>";
+    var title='<div class="card-body"><h5 class="card-title">'+element.title+'</h5>'
+    var text='<p class="card-text">'+element.text+'</p>'
+    var link='<a href="'+element.link+'" class="btn btn-orange">Go somewhere</a></div></div></div>'
+    customRows.innerHTML+=htmlString+header+title+text+link;
+  
+});
+
+
+
+//This below was the base html for each card from the inital prototype
+
+{/* <div class="card col-md-4 my-2">
+    <h5 class="card-header">Item 3</h5>
+
+    <div class="card-body">
+        <h5 class="card-title">Link's card</h5>
+        <p class="card-text">Meow Meow Meow</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+
+</div>  */}
+
+
---
+++ items.js
@@ -0,0 +1,50 @@
+var items = [
+
+//item1
+{
+    header: "Item1",
+    title: "Title of George's special card",
+    text: "Georgie pordgie",
+    link: "#"
+}
+//item2
+,{
+    header: "Item2",
+    title: "Title of jasons's card",
+    text: "Jason's Special Card",
+    link: "#"
+}
+//item3
+,{
+    header: "Item 3",
+    title: "Link's card",
+    text: "Meow Meow Meow",
+    link: "#"
+}
+//item4
+,{
+    header: "Item4",
+    title: "Lexi's Card",
+    text: "*Shits on floor*",
+    link: "#"
+}
+//item5
+,{
+    header: "Item5",
+    title: "Lolly's Title",
+    text: "Barks like a damn dog",
+    link: "#"
+}
+//item6
+,{
+    header: "Item6",
+    title: "Empty Card",
+    text: "Take my hand and stay with me my dear",
+    link: "#"
+}
+
+];
+
+
+
+export var items=items;
